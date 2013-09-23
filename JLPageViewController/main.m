f6b5c9c12b1070aed5f3f6ae8146fd8c3be317d7
jonathanlu813 <jonathanlu813@gmail.com>
2013-09-23 14:35:08 +0800

    project setup

---
+++ JLPageViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JLPageViewController
+//
+//  Created by Jonathan Lu on 23/9/13.
+//  Copyright (c) 2013 Jonathan Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JLAppDelegate class]));
+    }
+}
---
+++ JLPageViewControllerTests/JLPageViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  JLPageViewControllerTests.m
+//  JLPageViewControllerTests
+//
+//  Created by Jonathan Lu on 23/9/13.
+//  Copyright (c) 2013 Jonathan Lu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JLPageViewControllerTests : XCTestCase
+
+@end
+
+@implementation JLPageViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
